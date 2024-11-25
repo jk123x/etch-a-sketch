66f128d709fe51f7f2c816994a7c1f44d98cd045
jk123x <jay.kinkade@gmail.com>
2024-11-26 02:30:31 +1100

    Drew the grid and worked on basic styling

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-//Placeholder
+var div = document.createElement("div");
+div.style.width = "40px";
+div.style.height = "40px";
+div.style.background = "black";
+document.getElementById("container").appendChild(div);
+
+for (let i = 0; i < 255; i++) {
+    var div = document.createElement("div");
+    div.style.width = "40px";
+    div.style.height = "40px";
+    div.style.background = "black";
+    document.getElementById("container").appendChild(div);
+}
