d5267d5466bb43f7b0c90af5bede03441b11d7a0
jk123x <jay.kinkade@gmail.com>
2024-11-27 02:43:05 +1100

    mouseover working now, mouseout and button need work next

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-var div = document.createElement("div");
+var div = document.createElement("square");
 div.style.width = "40px";
 div.style.height = "40px";
 div.style.background = "black";
 document.getElementById("container").appendChild(div);
 
 for (let i = 0; i < 255; i++) {
-    var div = document.createElement("div");
+    var div = document.createElement("square");
     div.style.width = "40px";
     div.style.height = "40px";
     div.style.background = "black";
     document.getElementById("container").appendChild(div);
 }
+
+const gridItems = document.querySelectorAll("square");
+
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+}
+
+gridItems.forEach((div) => {
+div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = randomColor();
+//div.addEventListener("mouseout", () => {
+    //div.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${0.87})`
+});
+});
+//});
+
+//gridItems.forEach((div) => {
+    //div.addEventListener("mouseout", () => {
+        //div.style.background-color: rgba(0, 0, 0, 0.87);
+    //});
+    //});
